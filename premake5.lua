f90254cc8c70a3fe84394bcb1ba489d900d1d87c
shixiongfei <xiongfei.shi@icloud.com>
2021-02-28 23:19:43 +0800

    not need winmm lib

--- premake5.lua
+++ premake5.lua
@@ -40,7 +40,6 @@ solution ( "conf-test" )
     defines { "WIN32", "_WIN32", "_WINDOWS",
               "_CRT_SECURE_NO_WARNINGS", "_CRT_SECURE_NO_DEPRECATE",
               "_CRT_NONSTDC_NO_DEPRECATE", "_WINSOCK_DEPRECATED_NO_WARNINGS" }
-    links { "Winmm" }
 
   configuration ( "gmake" )
     warnings  "Default" --"Extra"
